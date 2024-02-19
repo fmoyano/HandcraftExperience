7c38b07467ceb327576a655e1b2f2f385df23139
Francis <fran.uma@gmail.com>
2024-02-19 18:53:52 +0100

    Removed unneded lib from compilation; added simple script to run the game.

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 pushd build
-cl /std:c11 /Zi /analyze /sdl /permissive- /W4 /guard:cf /I ../include ../src/*.c /Fe:main Kernel32.lib User32.lib shell32.lib dxgi.lib dxguid.lib d3d11.lib d3dcompiler.lib /link /LIBPATH:"..\lib\x86" /SUBSYSTEM:WINDOWS /MACHINE:X86
+cl /std:c11 /Zi /analyze /sdl /permissive- /W4 /guard:cf /I ../include ../src/*.c /Fe:main User32.lib shell32.lib dxgi.lib dxguid.lib d3d11.lib d3dcompiler.lib /link /LIBPATH:"..\lib\x86" /SUBSYSTEM:WINDOWS /MACHINE:X86
 popd
