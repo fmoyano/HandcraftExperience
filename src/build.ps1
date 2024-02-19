da3255192f9e5605ff04f1836ca84f24202a85d5
Francis <fran.uma@gmail.com>
2024-02-19 18:50:25 +0100

    Removed files that shouldn't be there.

--- src/build.ps1
+++
@@ -1,4 +0,0 @@
-pushd build
-cl /std:c11 /Zi /analyze /sdl /permissive- /W4 /guard:cf /I ../include ../src/*.c /Fe:main shell32.lib /link /LIBPATH:"..\lib\x86" /SUBSYSTEM:CONSOLE /MACHINE:X86
-popd
-sadasds
