4f45da6c89d861b0d4875f13b6dc74da65370d8f
Francis <fran.uma@gmail.com>
2024-03-11 18:56:19 +0100

    Removed static analysis to speed up compilation.

--- build.ps1
+++ build.ps1
@@ -7,5 +7,5 @@ fxc /Ges /Zi /T ps_4_1 .\data\shaders\PixelShader.hlsl /Ni /No /Fc .\data\shader
 Write-Output "Building app..."
 
 pushd build
-cl /std:c11 /Zi /analyze /sdl /permissive- /W4 /guard:cf /I ../include ../src/*.c /Fe:main User32.lib dxgi.lib dxguid.lib d3d11.lib /link /LIBPATH:"..\lib\x86" /SUBSYSTEM:WINDOWS /MACHINE:X86
+cl /std:c11 /Zi /sdl /permissive- /W4 /guard:cf /I ../include ../src/*.c /Fe:main User32.lib dxgi.lib dxguid.lib d3d11.lib /link /LIBPATH:"..\lib\x86" /SUBSYSTEM:WINDOWS /MACHINE:X86
 popd
